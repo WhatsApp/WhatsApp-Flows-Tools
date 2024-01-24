346476ce686f362bde58e2f0ed6ffe0f6c8172a5
Ankur Malik <ankurmalik@meta.com>
2024-01-24 02:43:03 -0800

    Added commented out method to validate flow token
    Reviewed By: Gaafar
    
    Differential Revision: D52909293
    
    fbshipit-source-id: 8aa562df645ececc52a779c9904cc41c29a1ca07

--- examples/endpoint/nodejs/basic/src/server.js
+++ examples/endpoint/nodejs/basic/src/server.js
@@ -36,6 +36,24 @@ app.post("/", async ({ body }, res) => {
   );
 
   console.log("💬 Decrypted Request:", decryptedBody);
+
+  // TODO: Uncomment this block and add your flow token validation logic.
+  // If the flow token becomes invalid, return HTTP code 427 to disable the flow and show the message in `error_msg` to the user
+  // Refer to the docs for details https://developers.facebook.com/docs/whatsapp/flows/reference/error-codes#endpoint_error_codes
+
+  /*
+  if (!isValidFlowToken(decryptedBody.flow_token)) {
+    const error_response = {
+      error_msg: `The message is no longer available`,
+    };
+    return res
+      .status(427)
+      .send(
+        encryptResponse(error_response, aesKeyBuffer, initialVectorBuffer)
+      );
+  }
+  */
+
   const screenResponse = await getNextScreen(decryptedBody);
   console.log("👉 Response to Encrypt:", screenResponse);
 
