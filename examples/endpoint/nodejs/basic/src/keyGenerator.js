3f2582681d767df6072e7a34c8a9267d1366168d
Gafi G <gafi@meta.com>
2024-01-30 09:07:31 -0800

    improve keyGenerator
    Differential Revision: D53208394
    
    fbshipit-source-id: ba15c4297a73cd7848384fcddb4e5a3393e9df4f

--- examples/endpoint/nodejs/basic/src/keyGenerator.js
+++ examples/endpoint/nodejs/basic/src/keyGenerator.js
@@ -5,23 +5,27 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import crypto from "crypto";
-
 /* The script will generate a public and private key pair and log the same in the console.
  * Copy paste the private key into your /.env file and public key should be added to your account.
  * For more details visit: https://developers.facebook.com/docs/whatsapp/flows/guides/implementingyourflowendpoint#upload_public_key
  *
  * Run this script using command below:
  *
- *             node src/keyGenerator.js
+ *             node src/keyGenerator.js {passphrase}
  *
  */
 
-const passphrase = "top secret";
+import crypto from "crypto";
 
-crypto.generateKeyPair(
-  "rsa",
-  {
+const passphrase = process.argv[2];
+if (!passphrase) {
+  throw new Error(
+    "Passphrase is empty. Please include passphrase argument to generate the keys like: node src/keyGenerator.js {passphrase}"
+  );
+}
+
+try {
+  const keyPair = crypto.generateKeyPairSync("rsa", {
     modulusLength: 2048,
     publicKeyEncoding: {
       type: "spki",
@@ -31,25 +35,21 @@ crypto.generateKeyPair(
       type: "pkcs1",
       format: "pem",
       cipher: "des-ede3-cbc",
-      passphrase: passphrase,
+      passphrase,
     },
-  },
-  (err, publicKey, privateKey) => {
-    // Handle errors and use the generated key pair.
-    if (err) {
-      return console.log("Error while creating public private key pair: ", err);
-    }
+  });
 
-    console.log(
-      "Succesfully created your public private key pair. Please copy paste the below values into your /.env files."
-    );
-    console.log("\n************* COPY PASSPHRASE BELOW *************\n");
-    console.log(passphrase);
-    console.log("\n************* COPY PUBLIC KEY BELOW *************\n");
-    console.log(publicKey);
-    console.log("\n************* COPY PUBLIC KEY ABOVE *************\n");
-    console.log("\n************* COPY PRIVATE KEY BELOW *************\n");
-    console.log(privateKey);
-    console.log("\n************* COPY PUBLIC KEY ABOVE *************");
-  }
-);
+  console.log(`Successfully created your public private key pair. Please copy the below values into your /.env file
+************* COPY PASSPHRASE & PRIVATE KEY BELOW TO .env FILE *************
+PASSPHRASE="${passphrase}"
+
+PRIVATE_KEY="${keyPair.privateKey}"
+************* COPY PASSPHRASE & PRIVATE KEY ABOVE TO .env FILE *************
+
+************* COPY PUBLIC KEY BELOW *************
+${keyPair.publicKey}
+************* COPY PUBLIC KEY ABOVE *************
+`);
+} catch (err) {
+  console.error("Error while creating public private key pair:", err);
+}
