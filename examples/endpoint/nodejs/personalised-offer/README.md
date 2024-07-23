0ee45d0518507fa229e941ba5198cb3c8f5bd236
Ladislav Gubík <ladislavg@meta.com>
2024-07-23 08:55:54 -0700

    Add Personalised Offer Glitch example
    Differential Revision: D60114806
    
    fbshipit-source-id: b6d38d29b83fc99fcca533a27273a86857944bbd

---
+++ examples/endpoint/nodejs/personalised-offer/README.md
@@ -0,0 +1,41 @@
+# WhatsApp Flow Endpoint Server
+
+This endpoint example is designed to be used with the [personalised offer Flow template](https://developers.facebook.com/docs/whatsapp/flows/gettingstarted/personalised-offer)
+
+## Flow Endpoint Docs
+
+Refer to the [docs here for implementing your Flow Endpoint](https://developers.facebook.com/docs/whatsapp/flows/guides/implementingyourflowendpoint)
+
+## ⚠️ WARNING ⚠️
+
+- This project is meant to be an example for prototyping only. It's not production ready.
+- When you remix (fork) this project on Glitch, your code is public by default, unless you choose to make it private (requires paid subscription to Glitch). Do not use this for any proprietary/private code.
+- Env variables are stored & managed by Glitch. Never use the private keys for your production accounts here. Create a temporary private key for testing on Glitch only and replace it with your production key in your own infrastructure.
+- Running this endpoint example on Glitch is completely optional and is not required to use WhatsApp Flows. You can run this code in any other environment you prefer.
+
+## Glitch Setup
+
+1. Create an account on Glitch to have access to all features mentioned here.
+2. Remix this project on Glitch.
+3. Create a private & public key pair for testing, if you haven't already, using the included script `src/keyGenerator.js`. Run the below command in the terminal to generate a key pair, then follow [these steps to upload the key pair](https://developers.facebook.com/docs/whatsapp/flows/guides/implementingyourflowendpoint#upload_public_key) to your business phone number.
+
+```
+node src/keyGenerator.js {passphrase}
+```
+
+4. Click on the file ".env" on the left sidebar, **then click on `✏️ Plain text` on top. Do not edit it directly from UI as it will break your key formatting.**
+5. Edit it with your private key and passphrase. Make sure a multiline key has the same line breaks like below. Env variables are only visible to the owner of the Glitch project. **Use a separate private key for testing only, and not your production key.**
+
+```
+PASSPHRASE="my-secret"
+
+PRIVATE_KEY="-----[REPLACE THIS] BEGIN RSA PRIVATE KEY-----
+MIIE...
+...
+...xyz
+-----[REPLACE THIS] END RSA PRIVATE KEY-----"
+```
+
+6. Use the new Glitch URL as your endpoint URL, eg: `https://project-name.glitch.me`. You can find this URL by clicking on `Share` on top right, then copy the `Live Site` URL.
+7. Edit `src/flow.js` with your logic to navigate between the Flow screens.
+8. Click on the `Logs` tab at the bottom to view server logs. The logs section also has a button to attach a debugger via Chrome devtools.
---
+++ examples/endpoint/nodejs/personalised-offer/src/server.js
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import express from "express";
+import { decryptRequest, encryptResponse, FlowEndpointException } from "./encryption.js";
+import { getNextScreen } from "./flow.js";
+import crypto from "crypto";
+
+const app = express();
+
+app.use(
+  express.json({
+    // store the raw request body to use it for signature verification
+    verify: (req, res, buf, encoding) => {
+      req.rawBody = buf?.toString(encoding || "utf8");
+    },
+  }),
+);
+
+const { APP_SECRET, PRIVATE_KEY, PASSPHRASE = "", PORT = "3000" } = process.env;
+
+/*
+Example:
+```-----[REPLACE THIS] BEGIN RSA PRIVATE KEY-----
+MIIE...
+...
+...AQAB
+-----[REPLACE THIS] END RSA PRIVATE KEY-----```
+*/
+
+app.post("/", async (req, res) => {
+  if (!PRIVATE_KEY) {
+    throw new Error(
+      'Private key is empty. Please check your env variable "PRIVATE_KEY".'
+    );
+  }
+
+  if(!isRequestSignatureValid(req)) {
+    // Return status code 432 if request signature does not match.
+    // To learn more about return error codes visit: https://developers.facebook.com/docs/whatsapp/flows/reference/error-codes#endpoint_error_codes
+    return res.status(432).send();
+  }
+
+  let decryptedRequest = null;
+  try {
+    decryptedRequest = decryptRequest(req.body, PRIVATE_KEY, PASSPHRASE);
+  } catch (err) {
+    console.error(err);
+    if (err instanceof FlowEndpointException) {
+      return res.status(err.statusCode).send();
+    }
+    return res.status(500).send();
+  }
+
+  const { aesKeyBuffer, initialVectorBuffer, decryptedBody } = decryptedRequest;
+  console.log("💬 Decrypted Request:", decryptedBody);
+
+  // TODO: Uncomment this block and add your flow token validation logic.
+  // If the flow token becomes invalid, return HTTP code 427 to disable the flow and show the message in `error_msg` to the user
+  // Refer to the docs for details https://developers.facebook.com/docs/whatsapp/flows/reference/error-codes#endpoint_error_codes
+
+  /*
+  if (!isValidFlowToken(decryptedBody.flow_token)) {
+    const error_response = {
+      error_msg: `The message is no longer available`,
+    };
+    return res
+      .status(427)
+      .send(
+        encryptResponse(error_response, aesKeyBuffer, initialVectorBuffer)
+      );
+  }
+  */
+
+  const screenResponse = await getNextScreen(decryptedBody);
+  console.log("👉 Response to Encrypt:", screenResponse);
+
+  res.send(encryptResponse(screenResponse, aesKeyBuffer, initialVectorBuffer));
+});
+
+app.get("/", (req, res) => {
+  res.send(`<pre>Nothing to see here.
+Checkout README.md to start.</pre>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
+
+function isRequestSignatureValid(req) {
+  if(!APP_SECRET) {
+    console.warn("App Secret is not set up. Please Add your app secret in /.env file to check for request validation");
+    return true;
+  }
+
+  const signatureHeader = req.get("x-hub-signature-256");
+  const signatureBuffer = Buffer.from(signatureHeader.replace("sha256=", ""), "utf-8");
+
+  const hmac = crypto.createHmac("sha256", APP_SECRET);
+  const digestString = hmac.update(req.rawBody).digest('hex');
+  const digestBuffer = Buffer.from(digestString, "utf-8");
+
+  if ( !crypto.timingSafeEqual(digestBuffer, signatureBuffer)) {
+    console.error("Error: Request Signature did not match");
+    return false;
+  }
+  return true;
+}
